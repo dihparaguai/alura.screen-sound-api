d029f9b025452760c19d8322ef720fbb0cdd9366
Dih Paraguai <diego.paraguai.dp@gmail.com>
2024-01-21 11:45:07 -0300

    inserido try catch , para caso de api indisponivel

--- ScreenSoundAPI/Program.cs
+++ ScreenSoundAPI/Program.cs
@@ -1,5 +1,15 @@
 ﻿using (HttpClient client = new HttpClient()) // cria uma variavel com o objeto HttpClient, para usarmos os metodos dele
 {
-    string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
-    Console.WriteLine(resposta);
+    try
+    {
+        string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+        Console.WriteLine(resposta);
+        // Para melhor entendimento: antes de usarmos a classe "HttpClient" na variavel "client", primeiro será executado tudo dentro das chaves, e depois é passado para a variavel "client"
+        // No caso acima: estamos requisitando ao endereço, todo o conteudo como string, para ser guardada na variavel resposta, e enquanto esta requisição não terminar, a linha vai esperar (await) até que o programa possa continuar
+        // O metodo assincrono: garante que tudo da requisição será lido, mesmo que o programa continue executando, enquanto o "await" diz para a variavel "resposta" esperar todo o conteudo ser lido antes de ser atribuido a ela
+
+    } catch (Exception ex)
+    {
+        Console.WriteLine($"Temos um probleminha: {ex.Message}"); // caso o endereço da requisição nao esteja disponivel, será gerado um erro, e este erro será capturado, então terá sua mensagem exibida no console
+    }
 }
--- ScreenSoundAPI/obj/Debug/net8.0/ScreenSoundAPI.AssemblyInfo.cs
+++ ScreenSoundAPI/obj/Debug/net8.0/ScreenSoundAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScreenSoundAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af4915829c04739fbaeb12e6266edcd370c44354")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScreenSoundAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScreenSoundAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
