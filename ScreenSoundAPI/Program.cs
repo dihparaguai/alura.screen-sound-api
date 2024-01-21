fb6317bd2ee71d7ea654d8500a00d275b82353b2
Dih Paraguai <diego.paraguai.dp@gmail.com>
2024-01-21 11:17:58 -0300

    criando requisição hppt com c sharp, de servidor que contem dados em json

---
+++ ScreenSoundAPI/Program.cs
@@ -0,0 +1,5 @@
+﻿using (HttpClient client = new HttpClient()) // cria uma variavel com o objeto HttpClient, para usarmos os metodos dele
+{
+    string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+    Console.WriteLine(resposta);
+}
